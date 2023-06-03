9b78d704383351878c5781c30427d79b6b9a2534
Nawabi-Hamza <hamza.nawabi119@gmail.com>
2023-06-03 15:32:47 +0430

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import FilterPage from './component/Filter';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">   
       <h1 className='p-1 my-3'>Hello This's Hamza</h1>
       {/* <FiltterPage /> */}
       <FilterPage />
